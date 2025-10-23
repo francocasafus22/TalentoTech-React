07b3b696819c8e991060b1cbdc6e0e0953bd0e82
francocasafus22 <francocasafus55@gmail.com>
2025-10-23 19:32:29 -0300

    add logo y hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,12 @@ export default function HomePage() {
 
   return (
     <>
-
+      <div className="flex w-full my-10 items-center justify-center text-center">
+        <div>
+          <h1 className="text-8xl text-blue-600">+TECH</h1>
+          <p className="text-4xl text-amber-50 max-w-xl">Los mejores smartphones y accesorios para acompañarte todos los días.</p>
+        </div>
+      </div>
       <ProductList category={"smartphones"} categoryName={"Celulares"}/>
       <ProductList category={"mobile-accessories"} categoryName={"Accesorios de Celular"}/>
       <ProductList category={"laptops"} categoryName={"Notebooks"}/>
